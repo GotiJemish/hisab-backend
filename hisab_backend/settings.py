a5beda03982d9080e30df85b9211110499a50b9d
Jemish Goti <jemish.goti@dcpltechnology.com>
2026-06-29 16:59:29 +0530

    feat: initialize Django project settings with database, CORS, and JWT authentication support

--- hisab_backend/settings.py
+++ hisab_backend/settings.py
@@ -182,8 +182,9 @@ assert os.getenv("EMAIL_PASSWORD"), "EMAIL_PASSWORD not set in .env"
 # For development, print emails to console
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv("EMAIL_ACCOUNT")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("EMAIL_ACCOUNT")
